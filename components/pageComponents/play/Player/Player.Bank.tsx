08014114f4cd3f3528623cf3ab0a9336b7a001ab
Heloi Neto <hey@heloineto.com>
2022-06-30 12:43:08 -0300

    updated PlayerBank

--- components/pageComponents/play/Player/Player.Bank.tsx
+++ components/pageComponents/play/Player/Player.Bank.tsx
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion';
 import Image from 'next/image';
+import classNames from 'clsx';
 
 interface Props {
   player: Player;
@@ -8,10 +9,13 @@ interface Props {
 const PlayerBank = ({ player }: Props) => {
   return (
     <motion.div
-      className="absolute top-0 left-5 flex h-32 w-32"
+      className={classNames(
+        'absolute  left-5 flex h-24 w-24 lg:h-28 lg:w-28',
+        player.type === 'player' ? '-top-6 lg:-top-5' : '-bottom-6 lg:-bottom-5'
+      )}
       whileHover={{ scale: 1.2 }}
     >
-      <div className="z-10 grid h-full w-full select-none place-items-center text-center font-display text-lg font-bold tracking-widest text-[#75231A]">
+      <div className="z-10 grid h-full w-full select-none place-items-center text-center font-display text-xs font-bold tracking-widest text-[#75231A] md:text-sm lg:text-base">
         <div>
           <div>{Number(player.money).toFixed(2).replace('.', ',')}</div>
           <div>R$</div>
