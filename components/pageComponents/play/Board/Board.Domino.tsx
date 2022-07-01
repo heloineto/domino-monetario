8617b6a674fe3810f9b64e4db0581b4efa70f60d
Heloi Neto <hey@heloineto.com>
2022-06-30 21:35:27 -0300

    feat: updated BoardDomino's height

--- components/pageComponents/play/Board/Board.Domino.tsx
+++ components/pageComponents/play/Board/Board.Domino.tsx
@@ -4,29 +4,35 @@ import classNames from 'clsx';
 
 interface Props extends ComponentProps<typeof motion.div> {
   rotation: Rotation;
+  width: number;
+  height: number;
   domino: [MoneyValue, MoneyValue];
 }
 
-const BoardDomino = ({ className, domino, rotation, ...motionDivProps }: Props) => {
+const BoardDomino = ({
+  className,
+  domino,
+  width,
+  height,
+  rotation,
+  ...motionDivProps
+}: Props) => {
   return (
-    <div
+    <motion.div
       className={classNames(className, 'flex items-center justify-center')}
       style={{
-        height: 160,
-        width: rotation ? 160 : 86.73,
+        width,
+        height,
+        rotate: rotation,
       }}
+      whileHover={{
+        scale: 1.2,
+        zIndex: 40,
+      }}
+      {...motionDivProps}
     >
-      <motion.div
-        style={{ rotate: rotation }}
-        whileHover={{
-          scale: 1.2,
-          zIndex: 40,
-        }}
-        {...motionDivProps}
-      >
-        <Domino className="h-40" domino={domino} />
-      </motion.div>
-    </div>
+      <Domino className="h-full w-full" domino={domino} />
+    </motion.div>
   );
 };
 
--- components/pageComponents/play/Board/Board.Dominos.tsx
+++ components/pageComponents/play/Board/Board.Dominos.tsx
@@ -1,6 +1,7 @@
 import SecondaryIconButton from '@components/elements/buttons/SecondaryIconButton';
 import { DragContext, GameContext } from '@lib/context';
 import { useElementSize } from '@lib/hooks';
+import { range } from '@lib/utils/math';
 import { isEmpty } from 'lodash';
 import { ArrowFatLineLeft, ArrowFatLineRight } from 'phosphor-react';
 import { useContext, useRef } from 'react';
@@ -36,6 +37,10 @@ const BoardDominos = ({}: Props) => {
 
   console.log({ boardSize });
 
+  const dominoHeight = range(0, 1280, 0, 160, boardSize.width);
+
+  console.log(dominoHeight, boardSize.width);
+
   if (!board) return null;
 
   return (
@@ -51,12 +56,17 @@ const BoardDominos = ({}: Props) => {
             edge={board.edges.start}
           />
         ) : (
-          <div className="flex-shrink-0" style={{ width: 160, height: 160 }} />
+          <div
+            className="flex-shrink-0"
+            style={{ width: dominoHeight, height: dominoHeight }}
+          />
         )}
         {board.boardDominos?.map(({ rotation, domino }) => (
           <BoardDomino
             className="flex-shrink-0"
             key={`${domino[0]}-${domino[1]}`}
+            height={dominoHeight}
+            width={rotation ? dominoHeight : dominoHeight / 2}
             domino={domino}
             rotation={rotation}
           />
